20721bc920b4ac11f2ffe17cdf5d7ff5f7fb9b7c
DESKTOP-18CC8V4\Daniel Álvarez <daniel10alra@gmail.com>
2021-11-06 17:25:21 -0600

    primeras clases. Perona y escritor de personas

---
+++ src/escritorPersona.h
@@ -0,0 +1,24 @@
+#ifndef ESCRITORPERSONA_H
+#define ESCRITORPERSONA_H
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+#include "persona.h"
+
+using namespace std;
+
+class EscritorPersona {
+
+    ofstream archivoSalida;
+
+    public:
+    EscritorPersona(string nombre);
+
+    void EscribirPersona(Persona &persona);
+    void Cerrar();
+
+};
+
+#endif
